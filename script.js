8bfa948984aa440a5c7c542fd4445591f9579ee0
Optimex <100842932+EminentDCoder@users.noreply.github.com>
2022-12-03 22:06:02 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+const collapse = document.querySelector(".collapse");
+const collapseToggle = collapse.querySelector('.bx');
+const root = document.querySelector(':root');
+const themeToggle = document.querySelector('.toggle span');
+const themeIcon = document.querySelector('.change-display i');
+
+
+collapse.addEventListener("click", () => {
+  if (container.classList.contains("collapses")) {
+    container.classList.remove("collapses");
+    collapseToggle.setAttribute('class', 'bx bx-chevron-left');
+  } else {
+    container.classList.add("collapses");
+    collapseToggle.setAttribute('class', 'bx bx-chevron-right');
+  }
+});
+
+
+
+// CHNAGE THE THEME DARK
+let themeLight = true;
+themeToggle.addEventListener('click', ()=>{
+  if(themeLight){
+    themeToggle.style.marginLeft = '0.9rem';
+    root.style.setProperty('--white-color', 'hsl(240, 4%, 25%)');
+    themeIcon.setAttribute('class', 'bx bx-sun');
+    themeLight = false;
+  } else if(!themeLight){
+    themeToggle.style.marginLeft = '0rem';
+    window.location.reload();
+    themeLight = true;
+  }
+})
