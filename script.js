113833878710c16de8793035696dec0ccfda8d30
Optimex <100842932+EminentDCoder@users.noreply.github.com>
2022-12-04 11:49:05 -0600

    c
    c

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const collapseToggle = collapse.querySelector('.bx');
 const root = document.querySelector(':root');
 const themeToggle = document.querySelector('.toggle span');
 const themeIcon = document.querySelector('.change-display i');
+const themeName = document.querySelector('.change-display h5');
+
 
 
 collapse.addEventListener("click", () => {
@@ -25,10 +27,12 @@ themeToggle.addEventListener('click', ()=>{
     themeToggle.style.marginLeft = '0.9rem';
     root.style.setProperty('--white-color', 'hsl(240, 4%, 25%)');
     themeIcon.setAttribute('class', 'bx bx-sun');
+    themeName.innerHTML = "Dark Mode";
     themeLight = false;
   } else if(!themeLight){
     themeToggle.style.marginLeft = '0rem';
     window.location.reload();
+    themeName.innerHTML = "Light Mode";
     themeLight = true;
   }
 })
